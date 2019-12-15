839e8f0bc59a39bcd8142d06380fb938c464d216
Luis Marcelo Chinea Rangel <alu0101118116@ull.edu.es>
2019-12-15 18:16:07 +0000

    Implementando el método de huella nutricional en Plato Dieta

--- lib/alimento/plato_dieta.rb
+++ lib/alimento/plato_dieta.rb
@@ -17,5 +17,30 @@ module Alimento
   	def to_s
   	  return "#{valor_calorico}kcal"
   	end
+
+    def huella_nutricional
+      indice_energia, indice_emisiones = 0.0
+
+      # Índice de la energía del plato
+      if valor_calorico < 670
+        indice_energia = 1.0
+      elsif valor_calorico > 830
+        indice_energia = 3.0
+      else
+        indice_energia = 2.0
+      end
+      
+      # Índice de las emisiones del plato
+      if valor_emisiones < 800
+        indice_emisiones = 1.0
+      elsif valor_emisiones > 1200
+        indice_emisiones = 3.0
+      else
+        indice_emisiones = 2.0
+      end
+
+      # Devolvemos la media
+      return ((indice_energia + indice_emisiones) / 2)
+    end
   end
 end
